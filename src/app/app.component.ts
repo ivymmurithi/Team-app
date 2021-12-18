041cbcfdc0d6d07ad4e7069f0fe246aaf3afc852
ivymmurithi <ivymurithi@gmail.com>
2021-12-18 16:33:13 +0300

    unfinished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'team-app';
+newMemberName = "";
+members: string[] = [];
+errorMessage = "";
+numberOfTeams: number | '' = "";
+
+onInput(member:  string) {
+  this.newMemberName = member;
+}
+
+onNumberOfTeamsInput(value: string) {
+  // Number is a wrapper object around number
+  this.numberOfTeams = Number(value);
+}
+
+addMember() {
+if(!this.newMemberName) {
+  this.errorMessage = "Name can't be empty";
+  return;
+}
+
+  this.errorMessage = "";
+  this.members.push(this.newMemberName);
+  this.newMemberName = "";
+}
+
+generateTeams () {
+  if(!this.numberOfTeams || this.numberOfTeams <= 0) {
+    return
+  }
+
+  const allMembers = [...this.members]
+
+   for (let i = 0; i < this.numberOfTeams; i++) {
+     const randomIndex = Math.floor(Math.random() * allMembers.length)
+   }
+  }
 }
